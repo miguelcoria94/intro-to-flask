7a5478d9f8e36dc34bebe225ef9cc08e7f9f67fa
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-22 03:24:22 -0600

    update config

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
+import os
+
 class Config(object):
     GREETING = 'Hello, superior students!'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-key-for-devs'
--- simple.py
+++ simple.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-app.config['greeting']='Hello, world!'
+app.config.from_object(Config)
 
 @app.route('/')
 def hello():
-    return f'<h1>{app.config["greeting"]}</h1>'
+    return f'<h1>{app.config["GREETING"]}</h1>'
