678c095ce9d0bee6f84849a0d887671b94eb2e37
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-22 03:25:28 -0600

    secret key

--- simple.py
+++ simple.py
@@ -4,6 +4,8 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+print('Secret key is: ', app.config["SECRET_KEY"] )
+
 @app.route('/')
 def hello():
     return f'<h1>{app.config["GREETING"]}</h1>'
